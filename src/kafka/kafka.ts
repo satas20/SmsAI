7c856acc5f48fceaed01e2040bb4e2d1d8e9f882
Ata Ayyıldız <84992313+satas20@users.noreply.github.com>
2025-04-07 21:11:30 +0300

    create the topic if not present

--- src/kafka/kafka.ts
+++ src/kafka/kafka.ts
@@ -10,6 +10,18 @@ const kafkaProducer = kafka.producer();
 const connectKafkaProducer = async () => {
   try {
     await kafkaProducer.connect();
+    const admin = kafka.admin();
+    await admin.connect();
+    const topics = await admin.listTopics();
+    if (!topics.includes('inbound-sms')) {
+      await admin.createTopics({
+        topics: [
+          { topic: 'inbound-sms', numPartitions: 1, replicationFactor: 1 },
+        ],
+      });
+      console.log('Created inbound-sms topic');
+    }
+    await admin.disconnect();
     console.log('Kafka producer connected.');
   } catch (error) {
     console.error('Error connecting Kafka producer:', error);
